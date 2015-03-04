671d2a7f81cb0334dde8431b0e3de37f22c25931
Michael Hrcek <mhrcek@imsa.edu>
2015-03-04 11:36:16 -0600

    Initial commit

---
+++ APCS Game of Sticks Server/src/apcs/gameofsticks/core/Boot.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package apcs.gameofsticks.core;
+
+/**
+ *
+ * @author mhrcek
+ */
+public class Boot {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
