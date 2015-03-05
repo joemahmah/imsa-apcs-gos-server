62d0261c79c524eafbbf1c57ffc8e1df138bf254
Toasterman9000 <akazenas@imsa.edu>
2015-03-05 13:02:03 -0600

    Updated Match.java

--- APCS Game of Sticks Server/src/apcs/gameofsticks/core/Match.java
+++ APCS Game of Sticks Server/src/apcs/gameofsticks/core/Match.java
@@ -10,5 +10,49 @@ package apcs.gameofsticks.core;
  * @author mhrcek
  */
 public class Match {
+    public static void run(){ // takes 2 AIs
+        int Player1RoundWins = 0;
+        int Player2RoundWins = 0;
+        int Player1GameWins = 0;
+        int Player2GameWins = 0;
+        System.out.println("Round 1, Fight!");
+        for(int g = 0; g < 1001; g++){
+            if(g % 2 == 0){
+                if(game(20, 3, true)){
+                    Player1GameWins++;
+                }else{
+                    Player2GameWins++;
+                }
+            }else{
+                if(game(20, 3, true)){
+                    Player1GameWins++;
+                }else{
+                    Player2GameWins++;
+                }
+            }
+            if(g == 700){
+                System.out.println("The winner of Round 1 is...");
+            }
+        }
+    }
     
+    public static boolean game(int numberOfSticks, int maxPlay, boolean player1Starts){ // returns true if player 1 wins
+        int sticksOnBoard = numberOfSticks;
+        if(player1Starts){
+            sticksOnBoard -= 1; // 1 = number of sticks player 1 chooses to take
+            if(sticksOnBoard == 0){
+                return false;
+            }
+        }
+        boolean whoseTurn = false;
+        while(sticksOnBoard > 0){
+            if(whoseTurn){
+                sticksOnBoard -= 1; // 1 = number of sticks player 1 chooses to take
+            }else{
+                sticksOnBoard += 2; // 2 = number of sticks player 2 chooses to take
+            }
+            whoseTurn = !whoseTurn;
+        }
+        return whoseTurn;
+    }
 }
