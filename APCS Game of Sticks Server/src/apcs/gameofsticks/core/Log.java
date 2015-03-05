d3810a0abb6435c7fa4175bb0a11450ca39d0569
Michael Hrcek <mhrcek@imsa.edu>
2015-03-05 07:13:55 -0600

    Added logic to log class. Now able to print to files.

--- APCS Game of Sticks Server/src/apcs/gameofsticks/core/Log.java
+++ APCS Game of Sticks Server/src/apcs/gameofsticks/core/Log.java
@@ -5,10 +5,44 @@
  */
 package apcs.gameofsticks.core;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author mhrcek
  */
 public class Log {
+    
+    public static synchronized void println(String fileName, String message){
+        PrintStream ps = null;
+        try {
+            File f = new File(fileName);
+            
+            ps = new PrintStream(f);
+            ps.append(message + "\n");
+        } catch (FileNotFoundException ex) {
+            ex.printStackTrace();
+        } finally {
+            ps.close();
+        }
+    }
+    
+    public static synchronized void print(String fileName, String message){
+        PrintStream ps = null;
+        try {
+            File f = new File(fileName);
+            
+            ps = new PrintStream(f);
+            ps.append(message);
+        } catch (FileNotFoundException ex) {
+            ex.printStackTrace();
+        } finally {
+            ps.close();
+        }
+    }
     
 }
