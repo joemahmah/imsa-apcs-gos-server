ace0687e7851cb9fbe7007715ea11b20ec77b98f
Toasterman9000 <akazenas@imsa.edu>
2015-03-06 12:34:37 -0600

    Finished up to round 4

--- APCS Game of Sticks Server/src/apcs/gameofsticks/core/Match.java
+++ APCS Game of Sticks Server/src/apcs/gameofsticks/core/Match.java
@@ -10,7 +10,7 @@ package apcs.gameofsticks.core;
  * @author mhrcek
  */
 public class Match {
-    public static void run(){ // takes 2 AIs
+    public static boolean run(){ // takes 2 AIs
         int Player1RoundWins = 0;
         int Player2RoundWins = 0;
         int Player1GameWins = 0;
@@ -34,6 +34,129 @@ public class Match {
                 System.out.println("The winner of Round 1 is...");
             }
         }
+        if(Player1GameWins > Player2GameWins){
+            Player1GameWins = 0;
+            Player2GameWins = 0;
+            Player1RoundWins++;
+            System.out.println("Player 1!");
+        }else{
+            Player1GameWins = 0;
+            Player2GameWins = 0;
+            Player2RoundWins++;
+            System.out.println("Player 2!");
+        }
+        System.out.println("Round 2, Fight!");
+        for(int g = 0; g < 1001; g++){
+            if(g % 2 != 0){
+                if(game(20, 3, true)){
+                    Player1GameWins++;
+                }else{
+                    Player2GameWins++;
+                }
+            }else{
+                if(game(20, 3, true)){
+                    Player1GameWins++;
+                }else{
+                    Player2GameWins++;
+                }
+            }
+            if(g == 700){
+                System.out.println("The winner of Round 2 is...");
+            }
+        }
+        if(Player1GameWins > Player2GameWins){
+            Player1GameWins = 0;
+            Player2GameWins = 0;
+            Player1RoundWins++;
+            System.out.println("Player 1!");
+        }else{
+            Player1GameWins = 0;
+            Player2GameWins = 0;
+            Player2RoundWins++;
+            System.out.println("Player 2!");
+        }
+        if(Player1RoundWins == 2){
+            System.out.println("Player 1 wins!");
+            return true;
+        }
+        if(Player2RoundWins == 2){
+            System.out.println("Player 2 wins!");
+            return false;
+        }
+        double randall = Math.random() * 100;
+        int startingNumberOfSticks = 50 + (int) randall;
+        System.out.println("Round 3, fight!");
+        for(int g = 0; g < 1001; g++){
+            if(g % 2 == 0){
+                if(game(startingNumberOfSticks, 3, true)){
+                    Player1GameWins++;
+                }else{
+                    Player2GameWins++;
+                }
+            }else{
+                if(game(startingNumberOfSticks, 3, true)){
+                    Player1GameWins++;
+                }else{
+                    Player2GameWins++;
+                }
+            }
+            if(g == 700){
+                System.out.println("The winner of Round 3 is...");
+            }
+        }
+        if(Player1GameWins > Player2GameWins){
+            Player1GameWins = 0;
+            Player2GameWins = 0;
+            Player1RoundWins++;
+            System.out.println("Player 1!");
+        }else{
+            Player1GameWins = 0;
+            Player2GameWins = 0;
+            Player2RoundWins++;
+            System.out.println("Player 2!");
+        }
+        randall = Math.random() * 100;
+        startingNumberOfSticks = 50 + (int) randall;
+        System.out.println("Round 4, fight!");
+        for(int g = 0; g < 1001; g++){
+            if(g % 2 != 0){
+                if(game(startingNumberOfSticks, 3, true)){
+                    Player1GameWins++;
+                }else{
+                    Player2GameWins++;
+                }
+            }else{
+                if(game(startingNumberOfSticks, 3, true)){
+                    Player1GameWins++;
+                }else{
+                    Player2GameWins++;
+                }
+            }
+            if(g == 700){
+                System.out.println("The winner of Round 4 is...");
+            }
+        }
+        if(Player1GameWins > Player2GameWins){
+            Player1GameWins = 0;
+            Player2GameWins = 0;
+            Player1RoundWins++;
+            System.out.println("Player 1!");
+        }else{
+            Player1GameWins = 0;
+            Player2GameWins = 0;
+            Player2RoundWins++;
+            System.out.println("Player 2!");
+        }
+        if(Player1RoundWins == 3){
+            System.out.println("Player 1 wins!");
+            return true;
+        }
+        if(Player2RoundWins == 3){
+            System.out.println("Player 2 wins!");
+            return false;
+        }
+        
+        return false;
     }
     
     public static boolean game(int numberOfSticks, int maxPlay, boolean player1Starts){ // returns true if player 1 wins
